556d6085ce83124e6f6970160f27ca5989bd3e89
Varsha-1710 <113410553+Varsha-1710@users.noreply.github.com>
2022-09-13 13:06:19 +0530

    Add files via upload

---
+++ Food-App/src/main/java/com/clvt/foodapp/FoodApp/repository/FoodOrderRepository.java
@@ -0,0 +1,9 @@
+package com.clvt.foodapp.FoodApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.clvt.foodapp.FoodApp.dto.FoodOrder;
+
+public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer> {
+
+}
