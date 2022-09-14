97bbf4de5c96ad0ddfdad773e2549f6be23c93a3
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2022-09-14 11:40:42 +0530

    Delete Food-App directory

--- Food-App/src/main/java/com/clvt/foodapp/FoodApp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clvt.foodapp.FoodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clvt.foodapp.FoodApp.dto.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
