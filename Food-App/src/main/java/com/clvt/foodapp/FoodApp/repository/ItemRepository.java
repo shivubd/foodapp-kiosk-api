9eca8fca9b39f27a26cc1ca878a1e72acaec0349
levi-a07 <113294384+levi-a07@users.noreply.github.com>
2022-09-14 12:09:53 +0530

    Delete Food-App directory

--- Food-App/src/main/java/com/clvt/foodapp/FoodApp/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clvt.foodapp.FoodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clvt.foodapp.FoodApp.dto.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer>{
-
-}
