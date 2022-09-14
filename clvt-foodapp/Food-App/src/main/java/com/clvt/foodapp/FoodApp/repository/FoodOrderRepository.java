bd19a795919ec19bd4cc835109c5d167102407c0
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2022-09-14 09:47:07 +0530

    Delete clvt-foodapp/Food-App directory

--- clvt-foodapp/Food-App/src/main/java/com/clvt/foodapp/FoodApp/repository/FoodOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.clvt.foodapp.FoodApp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clvt.foodapp.FoodApp.dto.FoodOrder;
-
-public interface FoodOrderRepository extends JpaRepository<FoodOrder, Integer> {
-
-}
